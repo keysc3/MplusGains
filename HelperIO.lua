80acb1b4fdcfd33553917b719a8967528853bafd
keysc3 <colin.j.keys@gmail.com>
2023-09-21 20:41:54 -0600

    Print out current season m+ dungeon names and timers.

--- HelperIO.lua
+++ HelperIO.lua
@@ -22,22 +22,7 @@ end
 
 print(bestOverAllScore)--]]
 
-local function FormatTimer(totalSeconds)
-    minutes = totalSeconds / 60
-    seconds = 60 * (minutes%1)
-    if seconds < 10 then
-        seconds = "0" .. seconds
-    end
-    return math.floor(minutes) .. ":" .. seconds
-end
-
-local function GetGeneralDungeonInfo()
-    local mapChallengeModeIDs = C_ChallengeMode.GetMapTable()
-    for i, map in ipairs(mapChallengeModeIDs) do
-        local name, id, timeLimit, texture, backgroundTexture = C_ChallengeMode.GetMapUIInfo(map)
-        print("MapInfo: " .. name ..  " " .. FormatTimer(timeLimit) .. "!")
-    end
-end
-
-
-GetGeneralDungeonInfo()
+--package.path = package.path .. ";../?.lua"
+local addonName, addon = ...
+print(string.format("Welcome to %s.", addonName))
+addon:GetGeneralDungeonInfo()
