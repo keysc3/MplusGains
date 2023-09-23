ee0ff5efd0052467397428497ac281d19ba99ec9
keysc3 <colin.j.keys@gmail.com>
2023-09-23 16:19:31 -0600

    UI base and initial button.

--- HelperIO.lua
+++ HelperIO.lua
@@ -1,12 +1,49 @@
---[[local frame = CreateFrame("Frame", nil, UIParent)
-frame:SetPoint("CENTER")
-frame:SetSize(200, 150)
+local addonName, addon = ...
+local frame = CreateFrame("Frame", nil, UIParent)
+frame:SetPoint("CENTER", nil, 0, 100)
+frame:SetSize(800, 600)
 
-frame.texture = frame:CreateTexture()
-frame.texture:SetAllPoints()
-frame.texture:SetColorTexture(0, 0, 0, 0.5)
+local function CreateNewTexture(red, green, blue, alpha, parent)
+    texture = parent:CreateTexture()
+    texture:SetAllPoints()
+    texture:SetColorTexture(red/255, green/255, blue/255, alpha)
+    return texture
+end
 
-frame:EnableMouse(true)
+frame.texture = CreateNewTexture(0, 0, 0, 0.5, frame)
+
+local btn = CreateFrame("Button", nil, frame, "UIPanelButtonTemplate")
+btn:SetPoint("CENTER")
+btn:SetSize(50, 60)
+btn:SetText("+10")
+
+local font = btn:GetNormalFontObject()
+font:SetTextColor(1, 1, 1 , 1)
+
+local isSelected = false
+local unselected = CreateNewTexture(66, 66, 66, 1, btn)
+local selected = CreateNewTexture(63, 81, 181, 1, btn)
+local selectedHighlight = CreateNewTexture(255, 255, 255, 0.1, btn)
+btn:SetNormalTexture(unselected)
+btn:SetHighlightTexture(selectedHighlight)
+
+btn:SetScript("OnClick", function(self, btn, down)
+    if(isSelected) then
+        self:SetNormalTexture(unselected)
+    else
+        self:SetNormalTexture(selected)
+    end
+    isSelected = not isSelected
+end)
+
+
+--[[local function ToggleButton(widget, button, down)
+    widget:SetNormalTexture(selected)
+end--]]
+--btn:SetText("Click me")
+
+
+--[[frame:EnableMouse(true)
 frame:SetMovable(true)
 frame:RegisterForDrag("LeftButton")
 frame:SetScript("OnDragStart", frame.StartMoving)
@@ -26,7 +63,6 @@ print(bestOverAllScore)--]]
 -- ((totaltime - runTime)/(totaltime - totaltime*0.6)) * 5 = bonusScore
 -- Overtime
 -- (abs(((totaltime - runTime))/(totaltime - totaltime*0.6)) * 5) + 5 = lostScore
-local addonName, addon = ...
 print(string.format("Welcome to %s.", addonName))
 addon:GetGeneralDungeonInfo()
 addon:GetPlayerDungeonBests()
