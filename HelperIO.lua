f1dc772afd523ef5f78035f7917dc60dd3541f7f
keysc3 <colin.j.keys@gmail.com>
2023-09-24 07:17:59 -0600

    Dungeon row, keystone button setup and initial button styling.

--- HelperIO.lua
+++ HelperIO.lua
@@ -1,40 +1,83 @@
 local addonName, addon = ...
-local frame = CreateFrame("Frame", nil, UIParent)
+
+local myButtons = {}
+local selected = { r = 63/255, g = 81/255, b = 181/255, a = 1 }
+local hover = { r = 255, g = 255, b = 255, a = 0.1 }
+local unselected = { r = 66/255, g = 66/255, b = 66/255, a = 1 }
+
+local myFont = CreateFont("Font")
+myFont:SetFont("Fonts\\FRIZQT__.TTF", 11, "OUTLINE, MONOCHROME")
+myFont:SetTextColor(1, 1, 1, 1)
+
+local frame = CreateFrame("Frame", "Main", UIParent)
 frame:SetPoint("CENTER", nil, 0, 100)
 frame:SetSize(800, 600)
+--frame:Hide()
+
+local frame1 = CreateFrame("Frame", "Row", frame)
+frame1:SetPoint("TOPLEFT")
+frame1:SetSize(600, 60)
 
 local function CreateNewTexture(red, green, blue, alpha, parent)
-    texture = parent:CreateTexture()
+    local texture = parent:CreateTexture()
     texture:SetAllPoints()
-    texture:SetColorTexture(red/255, green/255, blue/255, alpha)
+    texture:SetTexture("Interface\\buttons\\white8x8")
+    texture:SetVertexColor(red/255, green/255, blue/255, alpha)
     return texture
 end
 
-frame.texture = CreateNewTexture(0, 0, 0, 0.5, frame)
+local startingOffset = 0
 
-local btn = CreateFrame("Button", nil, frame, "UIPanelButtonTemplate")
-btn:SetPoint("CENTER")
-btn:SetSize(50, 60)
-btn:SetText("+10")
+local function CreateButton(keyLevel, xOffset, parentFrame)
+    local btn = CreateFrame("Button", parentFrame:GetName() .. "Button" .. "+" .. tostring(keyLevel), nil, "BackdropTemplate")
+    btn:SetParent(parentFrame)
+    btn:SetPoint("LEFT", xOffset, 0)
+    btn:SetSize(48, 58)
+    btn:SetBackdrop({
+        bgFile = "Interface\\buttons\\white8x8",
+        edgeFile = "Interface\\buttons\\white8x8",
+        edgeSize = 1,
+        insets = { left = 1, right = 1, top = 1, bottom = 1 },
+    })
+    btn:SetBackdropColor(unselected.r, unselected.g, unselected.b, unselected.a)
+    btn:SetText("+" .. keyLevel)
+    btn:SetHighlightTexture(CreateNewTexture(hover.r, hover.g, hover.b, hover.a, btn))
+    btn:SetNormalFontObject(myFont)
+    return btn
+end
 
-local font = btn:GetNormalFontObject()
-font:SetTextColor(1, 1, 1 , 1)
+local function CreateKeystoneLevelButton(keyLevel, btn)
+    local keystoneButtonObject = addon.CreateKeystoneButton(keyLevel, 260)
+    btn:SetScript("OnClick", function(self, btn, down)
+        if(keystoneButtonObject.isSelected) then
+            self:SetBackdropColor(unselected.r, unselected.g, unselected.b, unselected.a)
+        else
+            self:SetBackdropColor(selected.r, selected.g, selected.b, selected.a)
+        end
+        keystoneButtonObject.isSelected = not keystoneButtonObject.isSelected
+        print("Selected: " .. tostring(keystoneButtonObject.isSelected))
+    end)
+    return keystoneButtonObject
+end
 
-local isSelected = false
-local unselected = CreateNewTexture(66, 66, 66, 1, btn)
-local selected = CreateNewTexture(63, 81, 181, 1, btn)
-local selectedHighlight = CreateNewTexture(255, 255, 255, 0.1, btn)
-btn:SetNormalTexture(unselected)
-btn:SetHighlightTexture(selectedHighlight)
-
-btn:SetScript("OnClick", function(self, btn, down)
-    if(isSelected) then
-        self:SetNormalTexture(unselected)
-    else
-        self:SetNormalTexture(selected)
+local function CreateButtonRow(parentFrame, startingLevel, numButtons)
+    local xOffset = startingOffset
+    for i = 1, numButtons - 1 do
+        local button = CreateButton(startingLevel, xOffset, parentFrame)
+        button.keystoneButton = CreateKeystoneLevelButton(startingLevel, button)
+        xOffset = xOffset  + 48
+        startingLevel = startingLevel + 1
     end
-    isSelected = not isSelected
-end)
+    local children = {parentFrame:GetChildren()}
+
+    for i, child in ipairs(children) do
+        print(i, child:GetObjectType(), child:GetDebugName())
+    end
+end
+
+CreateButtonRow(frame1, 2, 10)
+frame1.texture = CreateNewTexture(40, 40, 40, 1, frame1)
+frame.texture = CreateNewTexture(0, 0, 0, 0.5, frame)
 
 
 --[[local function ToggleButton(widget, button, down)
@@ -63,6 +106,7 @@ print(bestOverAllScore)--]]
 -- ((totaltime - runTime)/(totaltime - totaltime*0.6)) * 5 = bonusScore
 -- Overtime
 -- (abs(((totaltime - runTime))/(totaltime - totaltime*0.6)) * 5) + 5 = lostScore
+
 print(string.format("Welcome to %s.", addonName))
 addon:GetGeneralDungeonInfo()
 addon:GetPlayerDungeonBests()
