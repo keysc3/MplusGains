a633c9f1fa09a98166e2917ef13e2e1993bc7b87
keysc3 <colin.j.keys@gmail.com>
2023-09-27 17:46:51 -0600

    Documentation.

--- KeystoneButton.lua
+++ KeystoneButton.lua
@@ -1,9 +1,15 @@
 local _, addon = ...
 
-function addon.CreateKeystoneButton(level, onTimeScore, button, index)
+--[[
+    CreateKeystoneButton - Creates a new keystone button object.
+    @param level - keystone level for the button.
+    @param button - button the object represents.
+    @param index - index of the button in its respective rows button table.
+    @return keystoneButton - created keystone button object
+--]]
+function addon.CreateKeystoneButton(level, button, index)
     local keystoneButton = {}
     keystoneButton.level = level
-    keystoneButton.onTimeScore = onTimeScore
     keystoneButton.mouseDown = false
     keystoneButton.button = button
     keystoneButton.index = index
